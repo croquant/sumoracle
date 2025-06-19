ca31cd22395340a91a1edf92b411e98fefd5a9f9
Maxime Desmet Vanden Stock <maximedesmet@yahoo.com>
2025-06-19 17:24:11 +0200

    Split view classes into dedicated modules

---
+++ app/views/division.py
@@ -0,0 +1,16 @@
+from django.views.generic import DetailView, ListView
+
+from ..models import Division
+
+
+class DivisionListView(ListView):
+    model = Division
+    template_name = "division_list.html"
+
+
+class DivisionDetailView(DetailView):
+    model = Division
+    template_name = "division_detail.html"
+    slug_field = "name"
+    slug_url_kwarg = "slug"
+    context_object_name = "division"
