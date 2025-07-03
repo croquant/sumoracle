579d1da7082f5ec4a4dc18f5046c37f8f174d591
Maxime Desmet Vanden Stock <maximedesmet@yahoo.com>
2025-07-03 09:21:57 +0200

    [sumoracle] Enhance index summary view

--- app/views/index.py
+++ app/views/index.py
@@ -1,5 +1,19 @@
 from django.views.generic import TemplateView
 
+from ..models import Basho, Division, Rikishi
+
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["division_count"] = Division.objects.count()
+        context["rikishi_count"] = Rikishi.objects.filter(
+            intai__isnull=True
+        ).count()
+        context["latest_basho"] = Basho.objects.order_by(
+            "-year",
+            "-month",
+        ).first()
+        return context
